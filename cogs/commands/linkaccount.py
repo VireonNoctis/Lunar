e33ac40ebe1adb300dd57586f9c3be3ef844398b
Vireon <on96852@gmail.com>
2026-09-11 22:25:12 +0400

    Update linkaccount.py

--- cogs/commands/linkaccount.py
+++ cogs/commands/linkaccount.py
@@ -13,7 +13,7 @@ from discord.ext import commands
 
 from cogs.utilities.database import db
 from cogs.utilities.emoji import EMOJI
-from cogs.utilities.generate_code import generate_code
+from cogs.utilities.generatecode import generate_code
 
 
 log = logging.getLogger("lunar.link")

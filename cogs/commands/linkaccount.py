922ebb10078e4c389b85df7168d52a59bc2b9d31
Vireon <on96852@gmail.com>
2026-09-11 17:49:26 +0400

    Update linkaccount.py

--- cogs/commands/linkaccount.py
+++ cogs/commands/linkaccount.py
@@ -11,7 +11,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from utilities.database import db
+from cogs.utilities.database import db
 from cogs.utilities.emoji import EMOJI
 from cogs.utilities.generate_code import generate_code
 

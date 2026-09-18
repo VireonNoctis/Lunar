a580265439cd53a61227c53a25f12f34c467050c
Vireon <on96852@gmail.com>
2026-09-18 15:01:25 +0400

    Update about.py

--- cogs/commands/about.py
+++ cogs/commands/about.py
@@ -8,7 +8,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from utilities.database import db
+from cogs.utilities.database import db
 from cogs.utilities.emoji import EMOJI
 from cogs.utilities.info import (
     BOT,
